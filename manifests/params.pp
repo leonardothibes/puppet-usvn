3cc75fbfb65bd27d61d47d7424a52b54062f53f8
Leonardo Thibes <eu@leonardothibes.com>
2014-06-13 16:23:51 -0300

    criando arquivo de parametros

---
+++ manifests/params.pp
@@ -0,0 +1,8 @@
+class usvn::params
+{
+	$download = 'https://github.com/leonardothibes/usvn/archive/'
+	$svnpath  = '/var/lib/usvn'
+	$dbhost   = 'localhost'
+	$dbuser   = 'usvn'
+	$dbname   = 'usvn'
+}
