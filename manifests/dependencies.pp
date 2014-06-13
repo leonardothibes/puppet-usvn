03d3face93d36fdf9a83295f69aaeff26b6d4891
Leonardo Thibes <eu@leonardothibes.com>
2014-06-13 16:19:03 -0300

    criando arquivo de dependencias

---
+++ manifests/dependencies.pp
@@ -0,0 +1,9 @@
+class usvn::dependencies
+{
+    if $osfamily != 'Debian' {
+        fail("Unsupported platform: ${osfamily}/${operatingsystem}")
+    }   
+    require wget
+    if !defined(Packages['php5'])       { package {'php5'      : ensure  => present} }
+    if !defined(Packages['subversion']) { package {'subversion': ensure  => present} }
+}
