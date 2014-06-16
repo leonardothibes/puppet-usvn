2e725ac0f2c88896c94e59ad889bb1d24e5a806e
Leonardo Thibes <eu@leonardothibes.com>
2014-06-16 17:37:50 -0300

    ajustes

--- manifests/install.pp
+++ manifests/install.pp
@@ -70,11 +70,16 @@ define usvn::install($version = $title, $svnpath)
 
 	# Granting write permissions
 	exec {'write-permissions-step-1':
-		command => "chmod 777 ${usvn::params::instdir}/current/public ${usvn::params::instdir}/current/config",
+		command => "chmod 777 ${usvn::params::instdir}/current/public",
 		require => Exec['copy-usvn'],
-		before  => File['write-permissions-step-2'],
+		before  => Exec['write-permissions-step-2'],
 	}
-	file {'write-permissions-step-2':
+	exec {'write-permissions-step-2':
+		command => "chmod 777 ${usvn::params::instdir}/current/config",
+		require => Exec['copy-usvn'],
+		before  => File['write-permissions-step-3'],
+	}
+	file {'write-permissions-step-3':
 		ensure  => present,
 		path    => "${usvn::params::instdir}/current/config/config.ini",
 		content => '[general]',
